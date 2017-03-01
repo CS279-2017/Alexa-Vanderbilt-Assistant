17e893654462707b8ca497dbe7feb23019e4d142
Arjun Dubashi <adubashi@gmail.com>
2017-03-01 14:03:28 -0600

    New restaurants

--- vanderbilt-assistant/src/data/restaurants.js
+++ vanderbilt-assistant/src/data/restaurants.js
@@ -1,16 +1,43 @@
 module.exports = {
-	"rand" : { 
+	"rand" : {
 	"0": "10:00am - 2:00pm",
-    "1": "7:00am - 3:00pm",  
+    "1": "7:00am - 3:00pm",
 	"2": "7:00am - 3:00pm and from 4:30pm - 7:30pm",
 	"3": "7:00am - 3:00pm and from 4:30pm - 7:30pm",
 	"4":"7:00am - 3:00pm",
 	"5":"7:00am - 3:00pm",
 	"6": "8:00am - 2:00pm"
     },
+	"ran" : {
+	"0": "10:00am - 2:00pm",
+    "1": "7:00am - 3:00pm",
+	"2": "7:00am - 3:00pm and from 4:30pm - 7:30pm",
+	"3": "7:00am - 3:00pm and from 4:30pm - 7:30pm",
+	"4":"7:00am - 3:00pm",
+	"5":"7:00am - 3:00pm",
+	"6": "8:00am - 2:00pm"
+    },
+	"rand dining center" : {
+	"0": "10:00am - 2:00pm",
+    "1": "7:00am - 3:00pm",
+	"2": "7:00am - 3:00pm and from 4:30pm - 7:30pm",
+	"3": "7:00am - 3:00pm and from 4:30pm - 7:30pm",
+	"4":"7:00am - 3:00pm",
+	"5":"7:00am - 3:00pm",
+	"6": "8:00am - 2:00pm"
+    },
+	"ran dining center" : {
+	"0": "10:00am - 2:00pm",
+	"1": "7:00am - 3:00pm",
+	"2": "7:00am - 3:00pm and from 4:30pm - 7:30pm",
+	"3": "7:00am - 3:00pm and from 4:30pm - 7:30pm",
+	"4":"7:00am - 3:00pm",
+	"5":"7:00am - 3:00pm",
+	"6": "8:00am - 2:00pm"
+	},
     "chef james" : {
     "0": "4:30pm - 7:30pm",
-    "1": "11:00am - 7:30pm",  
+    "1": "11:00am - 7:30pm",
 	"2": "11:00am - 7:30pm",
 	"3": "11:00am - 7:30pm",
 	"4": "11:00am - 7:30pm",
@@ -19,7 +46,7 @@ module.exports = {
     },
     "commons" : {
     "0": "10:00am - 8:00pm",
-    "1": "7:00am - 8:00pm",  
+    "1": "7:00am - 8:00pm",
 	"2": "7:00am - 8:00pm",
 	"3": "7:00am - 8:00pm",
 	"4": "7:00am - 8:00pm",
@@ -28,16 +55,34 @@ module.exports = {
     },
     "the kitchen" : {
     "0": "5:30pm - 7:30pm",
-    "1": "7:30am - 10:00am and from 5:30pm to 7:30pm  ",  
+    "1": "7:30am - 10:00am and from 5:30pm to 7:30pm  ",
 	"2": "7:30am - 10:00am and from 5:30pm to 7:30pm  ",
 	"3": "7:30am - 10:00am and from 5:30pm to 7:30pm  ",
 	"4": "7:30am - 10:00am and from 5:30pm to 7:30pm  ",
 	"5": "7:30am - 10:00am and from 5:30pm to 7:30pm  ",
 	"6": "Not open"
     },
+	"kissam" : {
+	"0": "5:30pm - 7:30pm",
+	"1": "7:30am - 10:00am and from 5:30pm to 7:30pm  ",
+	"2": "7:30am - 10:00am and from 5:30pm to 7:30pm  ",
+	"3": "7:30am - 10:00am and from 5:30pm to 7:30pm  ",
+	"4": "7:30am - 10:00am and from 5:30pm to 7:30pm  ",
+	"5": "7:30am - 10:00am and from 5:30pm to 7:30pm  ",
+	"6": "Not open"
+	},
+	"kiss sam" : {
+	"0": "5:30pm - 7:30pm",
+	"1": "7:30am - 10:00am and from 5:30pm to 7:30pm  ",
+	"2": "7:30am - 10:00am and from 5:30pm to 7:30pm  ",
+	"3": "7:30am - 10:00am and from 5:30pm to 7:30pm  ",
+	"4": "7:30am - 10:00am and from 5:30pm to 7:30pm  ",
+	"5": "7:30am - 10:00am and from 5:30pm to 7:30pm  ",
+	"6": "Not open"
+	},
     "last drop" : {
     "0": "3:00pm - 7:00pm",
-    "1": "7:00am - 7:00pm",  
+    "1": "7:00am - 7:00pm",
 	"2": "7:00am - 7:00pm",
 	"3": "7:00am - 7:00pm",
 	"4": "7:00am - 7:00pm",
@@ -46,16 +91,25 @@ module.exports = {
     },
     "pub" : {
     "0": "3:00pm - 9:00pm",
-    "1": "11:00am - 9:00pm",  
+    "1": "11:00am - 9:00pm",
 	"2": "11:00am - 9:00pm",
 	"3": "11:00am - 9:00pm",
 	"4": "11:00am - 9:00pm",
 	"5": "11:00am - 8:00pm",
 	"6": "not open"
     },
+	"the pub" : {
+	"0": "3:00pm - 9:00pm",
+	"1": "11:00am - 9:00pm",
+	"2": "11:00am - 9:00pm",
+	"3": "11:00am - 9:00pm",
+	"4": "11:00am - 9:00pm",
+	"5": "11:00am - 8:00pm",
+	"6": "not open"
+	},
     "rocket subs at towers": {
     "0": "11:00am - 10:00pm",
-    "1": "11:00am - 10:00pm",  
+    "1": "11:00am - 10:00pm",
 	"2": "11:00am - 10:00pm",
 	"3": "11:00am - 10:00pm",
 	"4": "11:00am - 10:00pm",
@@ -64,7 +118,7 @@ module.exports = {
     },
     "rocket subs at morgan": {
     "0": "11:00am - 10:00pm",
-    "1": "11:00am - 10:00pm",  
+    "1": "11:00am - 10:00pm",
 	"2": "11:00am - 10:00pm",
 	"3": "11:00am - 10:00pm",
 	"4": "11:00am - 10:00pm",
@@ -73,7 +127,7 @@ module.exports = {
     },
     "ro tiki": {
     "0": "not open",
-    "1": "11:00am - 8:00pm",  
+    "1": "11:00am - 8:00pm",
 	"2": "11:00am - 8:00pm",
 	"3": "11:00am - 8:00pm",
 	"4": "11:00am - 8:00pm",
@@ -82,7 +136,7 @@ module.exports = {
     },
     "au bon pain": {
     "0": "not open",
-    "1": "7:00am - 5:00pm",  
+    "1": "7:00am - 5:00pm",
 	"2": "7:00am - 5:00pm",
 	"3": "7:00am - 5:00pm",
 	"4": "7:00am - 5:00pm",
@@ -91,17 +145,16 @@ module.exports = {
     },
     "grins": {
     "0": "not open",
-    "1": "8:00am - 9:00pm",  
+    "1": "8:00am - 9:00pm",
 	"2": "8:00am - 9:00pm",
 	"3": "8:00am - 9:00pm",
 	"4": "8:00am - 9:00pm",
 	"5": "8:00am - 3:00pm",
 	"6": "not open"
-
     },
     "suzie's": {
     "0": "not open",
-    "1": "8:00am - 9:00pm",  
+    "1": "8:00am - 9:00pm",
 	"2": "8:00am - 9:00pm",
 	"3": "8:00am - 9:00pm",
 	"4": "8:00am - 9:00pm",
@@ -110,7 +163,7 @@ module.exports = {
     },
     "food for thought": {
     "0": "2:00pm - 8:00pm",
-    "1": "8:00am - 8:00pm",  
+    "1": "8:00am - 8:00pm",
 	"2": "8:00am - 8:00pm",
 	"3": "8:00am - 8:00pm",
 	"4": "8:00am - 8:00pm",
@@ -119,7 +172,7 @@ module.exports = {
     },
     "bamboo bistro": {
     "0": "not open",
-    "1": "8:00am - 8:00pm",  
+    "1": "8:00am - 8:00pm",
 	"2": "8:00am - 8:00pm",
 	"3": "8:00am - 8:00pm",
 	"4": "8:00am - 8:00pm",
@@ -128,7 +181,7 @@ module.exports = {
     },
     "pi and leaf" : {
     	"0": "5:00pm - 9:00pm",
-    	"1": "11:00am - 9:00pm",  
+    	"1": "11:00am - 9:00pm",
 		"2": "11:00am - 9:00pm",
 		"3": "11:00am - 9:00pm",
 		"4": "11:00am - 9:00pm",
@@ -137,7 +190,7 @@ module.exports = {
     },
     "mctyeire" : {
     	"0": "not open",
-    	"1": "7:00am - 10:00am and 6:00pm - 7:00pm",  
+    	"1": "7:00am - 10:00am and 6:00pm - 7:00pm",
 		"2": "7:00am - 10:00am and 6:00pm - 7:00pm",
 		"3": "7:00am - 10:00am and 6:00pm - 7:00pm",
 		"4": "7:00am - 10:00am and 6:00pm - 7:00pm",
@@ -146,7 +199,7 @@ module.exports = {
     },
     "common grounds" : {
     	"0": "24 hours",
-    	"1": "24 hours",  
+    	"1": "24 hours",
 		"2": "24 hours",
 		"3": "24 hours",
 		"4": "24 hours",
@@ -155,7 +208,7 @@ module.exports = {
     },
     "branscomb" : {
     	"0": "24 hours",
-    	"1": "24 hours",  
+    	"1": "24 hours",
 		"2": "24 hours",
 		"3": "24 hours",
 		"4": "24 hours",
@@ -164,7 +217,7 @@ module.exports = {
     },
     "campus store" : {
     	"0": "not open",
-    	"1": "	8:00am - 9:00pm",  
+    	"1": "	8:00am - 9:00pm",
 		"2": "	8:00am - 9:00pm",
 		"3": "	8:00am - 9:00pm",
 		"4": "	8:00am - 9:00pm",
@@ -173,7 +226,7 @@ module.exports = {
     },
     "towers market" : {
     	"0": "9:00am - 11:00pm",
-    	"1": "	24 hours",  
+    	"1": "	24 hours",
 		"2": "	24 hours",
 		"3": "	24 hours",
 		"4": "	24 hours",
@@ -183,7 +236,7 @@ module.exports = {
     },
     "kissam market" : {
     	"0": "9:00am - 11:00pm",
-    	"1": "	24 hours",  
+    	"1": "	24 hours",
 		"2": "	24 hours",
 		"3": "	24 hours",
 		"4": "	24 hours",
@@ -192,7 +245,7 @@ module.exports = {
     },
     "morgan market" : {
     	"0": "9:00am - 11:00pm",
-    	"1": "	24 hours",  
+    	"1": "	24 hours",
 		"2": "	24 hours",
 		"3": "	24 hours",
 		"4": "	24 hours",
