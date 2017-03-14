4016b8e0906cd6c9e650ca0f2a147f78ae4f9e85
Arjun Dubashi <adubashi@gmail.com>
2017-03-14 01:44:50 -0500

    Finish chef james specials

--- google.js
+++ google.js
@@ -11,15 +11,13 @@ var htmlstring;
 var horseman = new Horseman();
 async.series([
     function(callback){ 
-    console.log("Starting  first function");
+    console.log("Starting first function");
     horseman
 	  .userAgent('Mozilla/5.0 (Windows NT 6.1; WOW64; rv:27.0) Gecko/20100101 Firefox/27.0')
 	  .open('https://app.mymenumanager.net/vanderbilt/')
-	  .evaluate( function(){
-	  	  $('.tab[mealid="1"]').removeClass('selected');     
-	      $('.tab[mealid="2"]').addClass('selected');      
-	    })
-	  .html('.menu_blocks', 'output.html')
+	  .click('.tab[mealid="2"]')
+      .wait(5000)
+	  .html('#data_area', 'output.html')
 	  .then(function(body) {
 	    htmlstring = body;
 	    callback();
@@ -31,8 +29,32 @@ async.series([
     	callback();
     },
     function(callback){
+        
     	var $ = cheerio.load(htmlstring);
-    	console.log($)
+    	//console.log($)
+
+
+        $('#menu_area').filter(function(){
+            var data = $(this);
+            var menu_blocks = data.children()[0];
+            var blocksArray = menu_blocks['children'];   
+            //First child 
+            //console.log(blocksArray[0]['children'][0]['children'][0]['data']);
+            //Get Chef James
+
+            var meal = [];
+            for(var i = 0; i < blocksArray.length; i++){
+                //console.log(blocksArray[i]['children'][0]['children'][0]['data']);
+                if(blocksArray[i]['children'][0]['children'][0]['data'] == 'Chef James Bistro'){
+                    var informationArray = blocksArray[i]['children'][1]['children'][0]['children'];
+                    for(var i = 0; i < informationArray.length; i++){
+                        var info = informationArray[i]['children'][0]['data'];
+                        meal.push(info);
+                    }    
+                }
+            }
+            console.log(meal);
+        })
     	callback();
     }
 ]);
